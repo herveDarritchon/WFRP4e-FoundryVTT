363a7c251beb4d30a824398c887eb43528892935
Russell <russellthurman94@gmail.com>
2020-10-27 13:11:04 -0500

    Patch split issue

--- scripts/utility-wfrp4e.js
+++ scripts/utility-wfrp4e.js
@@ -67,6 +67,12 @@ class WFRP_Utility
    */
   static _prepareQualitiesFlaws(item, includeQualities = true)
   {
+    if (item.data.qualities.value == undefined || item.data.qualities.value == null)
+      item.data.qualities.value == ""
+
+    if (item.data.flaws.value == undefined || item.data.flaws.value == null)
+      item.data.flaws.value == ""
+      
     let qualities = item.data.qualities.value.split(",").map(function (item)
     {
       if (item)
